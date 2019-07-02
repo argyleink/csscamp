f39feba665eec10a6df215d7bbf7e6455af1d848
Adam Argyle <atom@argyleink.com>
2019-07-01 21:06:24 -0700

    slyd updates

--- app/slyd/slyd-observer.js
+++ app/slyd/slyd-observer.js
@@ -12,10 +12,12 @@ const slyd_observer = new IntersectionObserver(slides => {
     state.callbacks.forEach(cb =>
       cb(slyd))
 }, {
-  root: document.querySelector('main'),
+  root:       document.querySelector('main'),
   rootMargin: '0px',
-  threshold: 1,
+  threshold:  1,
 })
 
-document.querySelectorAll('slyd')
-  .forEach(slyd => slyd_observer.observe(slyd))
+Array.from(document.querySelectorAll('slyd'))
+  .filter((slyd, i) => i !== 0)
+  .forEach(slyd => 
+    slyd_observer.observe(slyd))
